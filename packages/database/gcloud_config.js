889bfe73d3d6ecc0f03628d534fea6e608ed8ce2
Jasper De Moor <jasperdemoor@gmail.com>
2019-03-03 14:17:08 +0100

    fix issues with keyfile

--- packages/database/gcloud_config.js
+++ packages/database/gcloud_config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  type: "service_account",
-  project_id: "sanguine-cloud",
-  private_key_id: process.env.SANGU_GCLOUD_PRIVATE_KEY_ID,
-  private_key: Buffer.from(process.env.SANGU_GCLOUD_PRIVATE_KEY, 'base64'),
-  client_email: "datastore-user@sanguine-cloud.iam.gserviceaccount.com",
-  client_id: "115199296874627602505",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/datastore-user%40sanguine-cloud.iam.gserviceaccount.com"
-};
+module.exports = JSON.parse(Buffer.from(process.env.GCLOUD_SERVICE_ACCOUNT, 'base64').toString());
